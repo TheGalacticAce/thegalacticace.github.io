1ad910e772f885344a3f0128bb4e7475cbab00f6
Harry <72170182+TheGalacticAce@users.noreply.github.com>
2023-04-01 00:24:46 -0700

    Update pireps.js

--- pireps.js
+++ pireps.js
@@ -1,9 +1,7 @@
 function textInput () {
     if (document.getElementById("textInput").value == "dhlv_login1") {
-        document.getElementById("p1").innerHTML = "<a href=\"https://airtable.com/shr9jz78b8PHFCnyg\">PIREP Form</a>"
-        document.getElementById("p2").innerHTML = "<a href=\"https://airtable.com/shriMXGeIgkjtW0o3\">CrewCenter</a>" 
+        document.getElementById("p1").innerHTML = "<a href=\"https://airtable.com/shr9jz78b8PHFCnyg\">PIREP Form</a>" 
     } else {
         document.getElementById("p1").innerHTML = "Incorrect Password"
-        document.getElementById("p2").innerHTML = ""
     }
 }
