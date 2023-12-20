6340bb1758a3aa8090830be151f98399a1312645
Harry <72170182+TheGalacticAce@users.noreply.github.com>
2023-12-19 16:11:11 -0800

    Delete pireps.js

--- pireps.js
+++
@@ -1,7 +0,0 @@
-function textInput () {
-    if (document.getElementById("textInput").value == "dhlv_login1") {
-        document.getElementById("p1").innerHTML = "<a href=\"https://airtable.com/shr9jz78b8PHFCnyg\">PIREP Form</a>" 
-    } else {
-        document.getElementById("p1").innerHTML = "Incorrect Password"
-    }
-}
